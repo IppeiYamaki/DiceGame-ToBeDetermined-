e8134c0aafed44948f2213be15c50791f888f107
ryuujouri <ryuukouri41@gmail.com>
2026-05-19 15:45:42 +0900

    dicemanagerchange

---
+++ Assets/Scripts/Liu/UI_DiceClickHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_DiceClickHandler : MonoBehaviour, IPointerClickHandler
+{
+
+    public DiceItem connectedDiceLogic;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (connectedDiceLogic != null)
+        {
+          
+            connectedDiceLogic.ToggleRiseFall();
+        }
+    }
+}

e19eb3c50bbfbb5f789e25af7d1a0271ade0404b
RaYez000 <ryousyun.1112314@icloud.com>
2026-06-23 17:42:38 +0900

    Map
    マップ追加。

--- Assets/Scripts/Hirakawa/EnemyDatabase.cs
+++ Assets/Scripts/Hirakawa/EnemyDatabase.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyDatabase", menuName = "Game/EnemyDatabase")]
+[CreateAssetMenu(fileName = "EnemyDatabase", menuName = "DiceGame/EnemyDatabase")]
 public class EnemyDatabase : ScriptableObject
 {
-    public EnemyData[] Enemies;
+    public EnemyDefinition[] Enemies;
 
-    public EnemyData GetRandom()
+    public EnemyDefinition GetRandom()
     {
         if (Enemies == null || Enemies.Length == 0) return null;
         return Enemies[Random.Range(0, Enemies.Length)];
